fa072c47d82a91f78423210154bccb01bfb38cc5
michasiok <michasio25@gmail.com>
2021-09-22 19:03:34 +0200

    cw.2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
+        String pierwszyString = "asd";
+        byte pierwszyByte = 127;
+        short pierwszyShort = 32767;
+        int pierwszyInt = 2147483647;
+        long pierwszyLong = 9223372036854775807L;
+        float pierwszyFloat = 111.12345678f;
+        double pierwszyDouble = 111.123456789456894;
+        boolean pierwszyBool = true;
+        char pierwszyChar = 'x';
 
+        int liczbaA = 40;
+        int liczbaB = 1;
+        System.out.println(liczbaA - liczbaB);
+        System.out.println(liczbaA + liczbaB);
+        System.out.println(liczbaA * liczbaB);
+        System.out.println(liczbaA / liczbaB);
+        System.out.println(liczbaA % liczbaB);
+
+        double liczbaX = 1.123;
+        double liczbaY = 3.321;
+        System.out.println(liczbaX - liczbaY);
+        System.out.println(liczbaX + liczbaY);
+        System.out.println(liczbaX * liczbaY);
+        System.out.println(liczbaX / liczbaY);
+        System.out.println(liczbaX % liczbaY);
     }
 }
