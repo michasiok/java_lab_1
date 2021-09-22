fd681cf420d7b3798ac7f93170672ecc8783961d
michasiok <michasio25@gmail.com>
2021-09-22 19:41:05 +0200

    cw.4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        int a = 1;
+        int b = 1;
+        int c = 5;
+        int d = 8;
 
+        System.out.println("((a == b) && !(c == d)) : " + ((a == b) && !(c == d)));
+        System.out.println("(a != b) && (c != d) : " + ((a != b) && (c != d)));
+        System.out.println("(a > b) || (c > d): " + ((a > b) || (c > d)));
     }
 }
