0e4ecf0f63ff081d3795220d2c6517a47eff4e9b
michasiok <michasio25@gmail.com>
2021-09-22 17:43:06 +0200

    cw.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Jestem stringiem ze znakiem nowej linii po sobie.");
+        System.out.print("Jestem stringiem bez niczego");
+        System.out.printf("Teraz %s wypisuje podane argumenty. %S", "o tu", " i moge byc formatowany");
     }
 }
