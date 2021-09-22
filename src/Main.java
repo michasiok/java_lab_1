70b5878ddbb9f3ec59fddcad11ba8b5cd7324c5c
michasiok <michasio25@gmail.com>
2021-09-22 19:17:47 +0200

    cw.3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Podaj imie: ");
+        String imie = scanner.next();
+
+        System.out.print("Podaj nazwisko: ");
+        String nazwisko = scanner.next();
+
+        System.out.print("Podaj wiek: ");
+        int wiek = scanner.nextInt();
+
+        System.out.print("Podaj numer indeksu: ");
+        long numerIndeksu = scanner.nextLong();
+
+        System.out.println("Imie: " + imie);
+        System.out.println("Nazwisko: " + nazwisko);
+        System.out.printf("Wiek: %d%nNumer indeksu: %d", wiek, numerIndeksu);
     }
 }
