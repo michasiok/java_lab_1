6e9bab7694a71b80e2193dc7c7d6fcf6a80418f8
michasiok <michasio25@gmail.com>
2021-09-22 21:24:37 +0200

    cw.6

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
+        double[] tablica = {1.2, 3.4, 5.6, 7.8, 9.10};
+        for (double t: tablica) {
+            System.out.println(t);
+        }
     }
 }
